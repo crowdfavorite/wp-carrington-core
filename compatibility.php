0543f850072d687a86c730139c7e642ddc27afd4
Gordon Brander <me@gordonbrander.com>
2009-02-16 20:27:06 +0000

    Added GPL header and attribution for WordPress code

--- compatibility.php
+++ compatibility.php
@@ -1,4 +1,20 @@
 <?php
+// This file is part of the Carrington Theme Framework for WordPress
+// http://carringtontheme.com
+//
+// Copyright (c) 2008-2009 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+//
+// Functions here without the cfct_ prefix are taken from WordPress 2.7 under the GPL
 
 /**
  * Displays classes for post div
