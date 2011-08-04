881715dfa116eae778d8ade991c73880428201dc
Gordon Brander <me@gordonbrander.com>
2011-08-04 22:11:15 +0000

    Replaced deprecated get_the_author_login with get_the_author_meta('login'). Replaced deprecated get_profile with get_user_by() and logic to make sure return value is the same.

--- utility.php
+++ utility.php
@@ -632,7 +632,7 @@ function cfct_choose_single_template_type($dir, $files, $filter) {
 function cfct_choose_single_template_author($dir, $files, $filter) {
 	$author_files = cfct_author_templates($dir, $files, $filter);
 	if (count($author_files)) {
-		$author = get_the_author_login();
+		$author = get_the_author_meta('login');
 		$file = cfct_filename_filter('author-'.$author.'.php', $filter);
 		if (in_array($file, $author_files)) {
 			return $file;
@@ -1371,7 +1371,8 @@ function cfct_cat_id_to_slug($id) {
  * 
 **/
 function cfct_username_to_id($username) {
-	return get_profile('ID', $username);
+	$user = get_user_by('ID', $username);
+	return (isset($user->ID) ? $user->ID : 0);
 }
 
 /**
